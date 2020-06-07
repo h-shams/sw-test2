62f9a18834f2df62cbc9e91f2953364884c004d6
heydar shams <shamsheidar.79@gmail.com>
2020-06-07 11:23:45 +0430

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,5 +8,5 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   console.log('fetched resourse')
-  // event.respondWith(fetch(event.request.url)
+  event.respondWith(fetch(event.request.url))
 })
